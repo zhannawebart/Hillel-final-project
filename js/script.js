44b148dc85e84cc15d0634da942b2d1668bb8bae
zhannawebart <ms.zhannavasileva@gmail.com>
2023-10-24 18:08:09 +0300

    modified pages contact,service, added popup

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-$('.burger').click(function (event) {
+/*----BURGER:---*/
+
+$('.burger').click(function () {
     $(this).toggleClass('active');
     $('nav').slideToggle();
 })
+
+
+/*----POPUP:----*/
+
+/*чтобы открыть попап: */
+$('.popup-button').click(function () {
+    $('.popup-wrapper').addClass('active');
+    const target = $(this).data('target');
+    $(target).addClass('active')
+})
+/*чтобы закрыть попап:*/
+$('.popup-wrapper, .popup-window .close').click(function (event) {
+    if (this === event.target) {
+        $('.popup-wrapper').removeClass('active');
+        $('.popup-window').removeClass('active');
+    }
+})
+
